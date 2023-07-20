962c1b619ebc96dd08c9356052cb80fb4b2a826f
Novan <l4novan132@gmail.com>
2023-07-20 09:10:46 +0700

    feat: create matrix 4 x 4 class

--- geometry.cpp
+++ geometry.cpp
@@ -46,6 +46,104 @@ public:
 
 };
 
+template <typename T>
+class Matrix44 {
+public:
+    T x[4][4] = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+
+    Matrix44() {}
+    Matrix44(T a, T b, T c, T d,
+             T e, T f, T g, T h,
+             T i, T j, T k, T l,
+             T m, T n, T o, T p) {
+        x[0][0] = a;
+        x[0][1] = b;
+        x[0][2] = c;
+        x[0][3] = d;
+
+        x[1][0] = e;
+        x[1][1] = f;
+        x[1][2] = g;
+        x[1][3] = h;
+
+        x[2][0] = i;
+        x[2][1] = j;
+        x[2][2] = k;
+        x[2][3] = l;
+
+        x[3][0] = m;
+        x[3][1] = n;
+        x[3][2] = o;
+        x[3][3] = p;
+    }
+
+    const T* operator[] (uint8_t i) const { return x[i]; }
+    T* operator[] (uint8_t i) { return x[i]; }
+
+    Matrix44<T> operator* (const Matrix44<T>& v) {
+        Matrix44 tmp;
+        multiply(*this, v, tmp);
+        return tmp;
+    }
+
+    static void multiply(const Matrix44<T>& a, const Matrix44<T>& b, Matrix44<T> c) {
+#if 0
+        for (uint8_t i = 0; i < 4; ++i) {
+            for (uint8_t j = 0; j < 4; ++j) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j] +
+                    a[i][2] + b[2][j] + a[i][3] * b[3][j];
+            }
+        }
+#else
+        const T* __restrict ap = &a.x[0][0];
+        const T* __restrict bp = &b.x[0][0];
+              T* __restrict cp = &c.x[0][0];
+
+        T a0, a1, a2, a3;
+
+        a0 = ap[0];
+        a1 = ap[1];
+        a2 = ap[2];
+        a3 = ap[3];
+
+        cp[0] = a0 * bp[0] + a1 * bp[4] + a2 * bp[8] + a3 * bp[12];
+        cp[1] = a0 * bp[1] + a1 * bp[5] + a2 * bp[9] + a3 * bp[13];
+        cp[2] = a0 * bp[2] + a1 * bp[6] + a2 * bp[10] + a3 * bp[14];
+        cp[3] = a0 * bp[3] + a1 * bp[7] + a2 * bp[11] + a3 * bp[15];
+
+        a0 = ap[4];
+        a1 = ap[5];
+        a2 = ap[6];
+        a3 = ap[7];
+
+        cp[4] = a0 * bp[0] + a1 * bp[4] + a2 * bp[8] + a3 * bp[12];
+        cp[5] = a0 * bp[1] + a1 * bp[5] + a2 * bp[9] + a3 * bp[13];
+        cp[6] = a0 * bp[2] + a1 * bp[6] + a2 * bp[10] + a3 * bp[14];
+        cp[7] = a0 * bp[3] + a1 * bp[7] + a2 * bp[11] + a3 * bp[15];
+
+        a0 = ap[8];
+        a1 = ap[9];
+        a2 = ap[10];
+        a3 = ap[11];
+
+        cp[8] = a0 * bp[0] + a1 * bp[4] + a2 * bp[8] + a3 * bp[12];
+        cp[9] = a0 * bp[1] + a1 * bp[5] + a2 * bp[9] + a3 * bp[13];
+        cp[10] = a0 * bp[2] + a1 * bp[6] + a2 * bp[10] + a3 * bp[14];
+        cp[11] = a0 * bp[3] + a1 * bp[7] + a2 * bp[11] + a3 * bp[15];
+
+        a0 = ap[12];
+        a1 = ap[13];
+        a2 = ap[14];
+        a3 = ap[15];
+
+        cp[12] = a0 * bp[0] + a1 * bp[4] + a2 * bp[8] + a3 * bp[12];
+        cp[13] = a0 * bp[1] + a1 * bp[5] + a2 * bp[9] + a3 * bp[13];
+        cp[14] = a0 * bp[2] + a1 * bp[6] + a2 * bp[10] + a3 * bp[14];
+        cp[15] = a0 * bp[3] + a1 * bp[7] + a2 * bp[11] + a3 * bp[15];
+#endif
+
+    }
+};
 
 template <typename T>
 T length(const Vec3<T>& v) {
